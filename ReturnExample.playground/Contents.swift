6471fe1b19907c1945a941a821241ba8139bc920
SemPruijs <sem@pruijs.nl>
2017-10-28 12:43:05 +0200

    Use array extension

--- ReturnExample.playground/Contents.swift
+++ ReturnExample.playground/Contents.swift
@@ -1,42 +1,25 @@
 import Foundation
 
-extension String {
-    func uppercasedToShit() -> String {
-        return self.uppercased() + "💩"
-    }
-}
+//randaaaaaaaam!!!
 
 extension Array {
-    func randomItem() -> Element {
+    
+    
+    func randomElement() -> Element {
         return self[Int(arc4random() % UInt32(self.count))]
     }
 }
 
-
-"hahahahaha".uppercasedToShit()
-
-// Onderwerp
 let naam = ["harry", "bert", "henk", "sebastiaan"]
-let randomIndex = Int(arc4random() % UInt32(naam.count))
-print(naam[randomIndex])
-print(naam.randomItem())
-
-// Lijdend voorwerp
 let voorwerp = ["stoep", "dinosaurus", "klokken", "lampen"]
-let randomvoorwerp = Int(arc4random() % UInt32(voorwerp.count))
-print(voorwerp[randomIndex])
-
-// Beroep
 let watJeKanDoen = ["inspeckteur", "zitter", "liefhebber", "visser"]
-let randomwatJeKanDoen = Int(arc4random() % UInt32(watJeKanDoen.count))
-print(watJeKanDoen[randomIndex])
 
+func voledigeberoep() -> String {
+    return naam.randomElement() + " de " + voorwerp.randomElement() + " " + watJeKanDoen.randomElement()
+}
 
-let voledigeberoep = naam[randomIndex] + " de " + voorwerp[randomIndex] + " " + watJeKanDoen[randomIndex]
-
-print(voledigeberoep)
-
-
-
+for _ in 0...10 {
+    print(voledigeberoep())
+}
 
 
