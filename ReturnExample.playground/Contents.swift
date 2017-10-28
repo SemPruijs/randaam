c98122b36069c37620f4e2d21d4d0b5b152a5275
SemPruijs <sem@pruijs.nl>
2017-10-28 12:53:40 +0200

    Update Dutch to English

--- ReturnExample.playground/Contents.swift
+++ ReturnExample.playground/Contents.swift
@@ -4,7 +4,6 @@ import Foundation
 
 extension Array {
     
-    
     func randomElement() -> Element {
         return self[Int(arc4random() % UInt32(self.count))]
     }
@@ -14,12 +13,12 @@ let naam = ["harry", "bert", "henk", "sebastiaan"]
 let voorwerp = ["stoep", "dinosaurus", "klokken", "lampen"]
 let watJeKanDoen = ["inspeckteur", "zitter", "liefhebber", "visser"]
 
-func voledigeberoep() -> String {
+func fullJob() -> String {
     return naam.randomElement() + " de " + voorwerp.randomElement() + " " + watJeKanDoen.randomElement()
 }
 
 for _ in 0...10 {
-    print(voledigeberoep())
+    print(fullJob())
 }
 
 
