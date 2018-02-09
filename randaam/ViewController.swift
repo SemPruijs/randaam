7bf430d6c35301dcf9df9f0cfbfcb4f1c2019019
SemPruijs <sem@pruijs.nl>
2018-02-09 10:58:39 +0100

    switch to switch :)

--- randaam/ViewController.swift
+++ randaam/ViewController.swift
@@ -25,20 +25,27 @@ class ViewController: UIViewController {
 
     @IBAction func screenTapped(_ sender: UITapGestureRecognizer) {
         let salary = personInfoGenerator.generateSalary()
-        if salary >= 990 {
-            salaryLabel.textColor = UIColor.purple
-        } else if salary >= 900 {
-            salaryLabel.textColor = UIColor.cyan
-        } else if salary >= 600 {
-            salaryLabel.textColor = UIColor.green
-        } else if salary >= 400 {
-            salaryLabel.textColor = UIColor.yellow
-        } else if salary >= 200 {
-            salaryLabel.textColor = UIColor.orange
-        } else if salary >= 0 {
-            salaryLabel.textColor = UIColor.red
+        
+       
+        
+        switch salary {
+        case 1..<200:
+                salaryLabel.textColor = #colorLiteral(red: 0.8358970207, green: 0.1556696349, blue: 0, alpha: 1)
+        case 200..<400:
+                salaryLabel.textColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        case 400..<600:
+                salaryLabel.textColor = #colorLiteral(red: 0.5183701748, green: 0.6004124365, blue: 0, alpha: 1)
+        case 600..<900:
+                salaryLabel.textColor = #colorLiteral(red: 0.2457480216, green: 1, blue: 0, alpha: 1)
+        case 900..<990:
+                salaryLabel.textColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        case 990..<999:
+                salaryLabel.textColor = #colorLiteral(red: 0.9662646678, green: 0.09260331458, blue: 0.9686274529, alpha: 1)
+    
+        default: salaryLabel.textColor = #colorLiteral(red: 0.4190762824, green: 0.005558046088, blue: 0.3883012486, alpha: 1)
+            
         }
-     
+        
         fulljobLabel.text = personInfoGenerator.generateJob()
         salaryLabel.text = "€\(salary)/week"
         
