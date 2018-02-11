57686ae7f8cef217005901c7def5d7f09b035f0f
SemPruijs <sem@pruijs.nl>
2018-02-11 14:19:34 +0100

    Add docs folder Add more names

---
+++ randaam-watch/randaam/randaam WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  randaam WatchKit Extension
+//
+//  Created by Sem Pruijs on 11-02-18.
+//  Copyright © 2018 Sem Pruijs. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
