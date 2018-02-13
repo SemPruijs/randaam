961b7766e0f7be86f34bcaaabc2f43ebee9a0d97
SemPruijs <sem@pruijs.nl>
2018-02-13 15:55:46 +0100

    add welkom screen

--- randaam/ViewController.swift
+++ randaam/ViewController.swift
@@ -69,7 +69,7 @@ class ViewController: UIViewController {
             logoLabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         case 990..<999:
             raretyLabel.text = "episch!!"
-            self.view.backgroundColor = #colorLiteral(red: 0, green: 0.6198842005, blue: 0.005984226446, alpha: 1)
+            self.view.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0.9229307487, alpha: 1)
             fulljobLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
             ageLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
             raretyLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
@@ -89,9 +89,9 @@ class ViewController: UIViewController {
         salaryLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         } else if view.backgroundColor ==  #colorLiteral(red: 0.04830179749, green: 0.0630759631, blue: 0.433653236, alpha: 1)  && salaryLabel.textColor ==  #colorLiteral(red: 0.4190762824, green: 0.005558046088, blue: 0.3883012486, alpha: 1) {
                 salaryLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        } else if view.backgroundColor ==  #colorLiteral(red: 0, green: 0.6198842005, blue: 0.005984226446, alpha: 1)  && salaryLabel.textColor ==  #colorLiteral(red: 0.5183701748, green: 0.6004124365, blue: 0, alpha: 1) {
+        } else if view.backgroundColor ==  #colorLiteral(red: 1, green: 0, blue: 0.9229307487, alpha: 1)  && salaryLabel.textColor ==  #colorLiteral(red: 0.9662646678, green: 0.09260331458, blue: 0.9686274529, alpha: 1) {
             salaryLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        }  else if view.backgroundColor ==  #colorLiteral(red: 0, green: 0.6198842005, blue: 0.005984226446, alpha: 1)  && salaryLabel.textColor ==  #colorLiteral(red: 0.2457480216, green: 1, blue: 0, alpha: 1) {
+        }  else if view.backgroundColor ==  #colorLiteral(red: 1, green: 0, blue: 0.9229307487, alpha: 1)  && salaryLabel.textColor ==  #colorLiteral(red: 0.4190762824, green: 0.005558046088, blue: 0.3883012486, alpha: 1) {
             salaryLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         }
         
